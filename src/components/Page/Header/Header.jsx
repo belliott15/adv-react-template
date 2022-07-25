7c6a53e17495b21d66206f23382dc64d1af8fa81
Beau Elliott <belliott1516@gmail.com>
2022-07-25 16:09:05 -0700

    added structure to header including navigation and menu

--- src/components/Page/Header/Header.jsx
+++ src/components/Page/Header/Header.jsx
@@ -1,7 +1,21 @@
+import Menu from './Menu';
+import Navigation from './Navigation';
 
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div>
+        <Menu />
+      </div>
+
+      <div>
+        App Name
+      </div>
+
+      <div>
+        <Navigation />
+      </div>
+    </header>
   );
 }
--- src/components/Page/Header/Navigation.jsx
+++ src/components/Page/Header/Navigation.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Navigation() {
   return (
     <nav>
-      <Link>Home</Link>
-      <Link>Dungeon</Link>
-      <Link>Lair</Link>
+      <Link to="/">Home</Link>
+      <Link to="dungeon">Dungeon</Link>
+      <Link to="lair">Lair</Link>
     </nav>
   );
 }
