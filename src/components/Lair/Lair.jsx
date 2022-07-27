ed817ae6d39ec69156199a5bbb68faefd251f788
Beau Elliott <belliott1516@gmail.com>
2022-07-27 08:43:54 -0700

    added lair css and built out form on lairjsx

--- src/components/Lair/Lair.jsx
+++ src/components/Lair/Lair.jsx
@@ -1,6 +1,37 @@
 
+
 export default function Lair() {
   return (
-    <div>My Lair Lady</div>
+    <section>
+      <form>
+        <label>
+          Name:
+          <input></input>
+        </label>
+
+        <label>
+          Class:
+          <select>
+            <option>This</option>
+            <option>That</option>
+            <option>Druid</option>
+            <option>Another Thing</option>
+            <option>Rogue</option>
+            <option>Paladin</option>
+            <option>Basic Bitch</option>
+          </select>
+        </label>
+
+        <label>
+          Bio:
+          <textarea></textarea>
+        </label>
+
+        <label>
+          Race:
+          <input></input>
+        </label>
+      </form>
+    </section>
   );
 }
