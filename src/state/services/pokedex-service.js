5e6431bf32ec79f5827a0d64dc6701d0d61d560b
Beau Elliott <belliott1516@gmail.com>
2022-07-29 08:24:19 -0700

    added getpokedex and get types to services

--- src/state/services/pokedex-service.js
+++ src/state/services/pokedex-service.js
@@ -1,5 +1,6 @@
 const TYPES_URL = `${process.env.API_URL}/types`;
 
+//catch all get function that pulls from the api
 export default async function get(url) {
   const res = await fetch(url);
   const body = await res.json();
@@ -10,3 +11,12 @@ export default async function get(url) {
   };
 }
 
+//gets all pokemon
+export async function getPokedex(){
+  return await get(`${process.env.API_URL}`);
+}
+
+//gets the types of pokemon and their counts
+export async function getTypes(){
+  return await get(TYPES_URL);
+}
