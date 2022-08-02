f8dec285b80a18264a336ed3d7c61593149c55ca
Beau Elliott <belliott1516@gmail.com>
2022-08-02 16:13:31 -0700

    added functionality to add a family

--- src/components/FuzzyBunny/AddFamily.jsx
+++ src/components/FuzzyBunny/AddFamily.jsx
@@ -1,6 +1,31 @@
+import { useState } from 'react';
+import { useFamilyActions } from '../../state/hooks/fuzzyBunny';
+import { InputControl, FormButton } from '../Forms/FormControls';
+import styles from './AddFamily.css';
 
 export default function AddFamily() {
+  const { add } = useFamilyActions();
+  const [name, setName] = useState();
+
+  const handleChange = ({ target }) => setName(target.value);
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    await add({ name });
+    setName('');
+  };
+
   return (
-    <div>AddFamily</div>
+    <form className={styles.AddFamily} onSubmit={handleSubmit}>
+      <InputControl 
+        label="add a family"
+        name="family"
+        value={name}
+        onChange={handleChange}
+      />
+      <FormButton>
+        +
+      </FormButton>
+    </form>
   );
 }
--- src/components/FuzzyBunny/Families.jsx
+++ src/components/FuzzyBunny/Families.jsx
@@ -1,9 +1,11 @@
 import FamilyList from './FamilyList.jsx';
 import styles from './Families.css';
+import AddFamily from './AddFamily.jsx';
 
 export default function Families() {
   return (
     <section className={styles.FamilyList}>
+      <AddFamily />
       <FamilyList />
     </section>
   );
