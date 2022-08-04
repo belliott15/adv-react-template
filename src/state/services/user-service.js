13efa1165042891dd4e6215a858e6cdd5c69d8be
Beau Elliott <belliott1516@gmail.com>
2022-08-04 16:20:57 -0700

    added userContext and profile functions to services

--- src/state/services/user-service.js
+++ src/state/services/user-service.js
@@ -1,14 +1,51 @@
 import client from './supabase-client';
 
+//gets the user from supabase
 export function getUser(){ 
   return client.auth.user();
 }
 
+//checks the supabase users table and logs in current users
 export async function signIn(credentials){
   return await client.auth.signIn(credentials);
 }
 
+//creates a new user in the users table
 export async function signUp(credentials){
   return await client.auth.signUp(credentials);
 }
 
+//gets a users profile from the user_profile table in supabase
+export async function getProfile(){
+  const user = getUser();
+
+  return await client
+    .from('user_profiles')
+    .select()
+    .eq('id', user.id)
+    .single();
+}
+
+//updates profile
+export async function upsertProfile(profile){
+  const response = await client
+    .from('user_profiles')
+    .upsert(profile)
+    .eq('id', profile.id)
+    .single();
+
+  return response;
+}
+
+const PROFILE = 'profile';
+
+//gets the stored local profile on 
+export async function getLocalProfile(){
+  const json = localStorage.getItem(PROFILE);
+  if(!json) return null;
+  try{
+    return JSON.parse(json);
+  }catch (err) {
+    localStorage.removeItem(PROFILE);
+  }
+}
